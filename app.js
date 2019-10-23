b5c01e4c68da4f4580447a52c68386b15407d228
sd <dfghxdf@dgzs.com>
2019-10-23 10:31:08 -0300

    first now

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use('/', routers);
+    app.use('/', routers);
 conectaMongodb();
 
 app.listen(3000, () => console.log('App Online'));
